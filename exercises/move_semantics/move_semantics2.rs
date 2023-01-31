79c49cbbe489612815f82e5363157ae41f93bcd8
jesseaam <18119820+jesseaam@users.noreply.github.com>
2023-01-31 14:53:02 -0500

    code updates

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,12 +2,15 @@
 // Make me compile without changing line 13 or moving line 10!
 // Execute `rustlings hint move_semantics2` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
+// approach 1
+// just make another copy of vec0 in a dif var
+/*
 fn main() {
-    let vec0 = Vec::new();
+    let vec0: Vec<i32> = Vec::new();
+    let vec0b = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec(vec0b);
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
@@ -26,3 +29,55 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
 
     vec
 }
+*/
+
+// approach 2
+// make a copy (clone) of referenced input vec in fill_vec function
+fn main() {
+    let vec0 = Vec::new();
+
+    let mut vec1 = fill_vec(&vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec1.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+}
+
+fn fill_vec(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+
+
+// approach 3
+// make fill_vec modify vec directly (and not return anything)
+/* 
+fn main() {
+    let mut vec0: Vec<i32> = Vec::new();
+
+    fill_vec(&mut vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec0.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+}
+
+fn fill_vec(vec: &mut Vec<i32>)  {
+    let mut vec = vec;
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+}
+*/
